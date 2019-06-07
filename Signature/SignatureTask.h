68be95443a723280540b25899c4314391cc2efd8
sesk <kulikovss@gmail.com>
2019-06-07 23:47:33 +0300

    Update SignatureTask.h
    read file separately

--- Signature/SignatureTask.h
+++ Signature/SignatureTask.h
@@ -21,12 +21,9 @@ class SignatureTask
 	streampos _extra_block;
 	unsigned long long _blocksize;
 	unsigned long long _extra_block_size;
-	const char* _in_file;
+	char* _in_buffer;
 	char* _out_buffer;
 	streampos _processed_blocks;
-	streampos _in_offset;
-
-	std::unique_ptr<char[]> _in_buffer;
 
 	int calculateHash(const char* block, unsigned long long blocksize);
 
@@ -40,19 +37,17 @@ public:
 		          streampos& extra_block, // number of extra block (last block with different size if it exists)
 		          unsigned long long blocksize,         // size of usual blocks
 		          unsigned long long extra_block_size,  // size of extra block
-		          const char* in_file,        // file name for input file
+		          char* in_buffer,        // input buffer
 		          char* out_buffer,       // buffer to output data
-		          streampos& processed_blocks, // processed blocks (already)
-		          streampos in_offset)    // offset of input   
+		          streampos& processed_blocks) // processed blocks (already)
 		: _begin_block(begin_block), 
 		_end_block(end_block), 
 		_extra_block(extra_block), 
 		_blocksize(blocksize), 
 		_extra_block_size(extra_block_size),
-		_in_file(in_file), 
+		_in_buffer(in_buffer), 
 		_out_buffer(out_buffer), 
-		_processed_blocks(processed_blocks), 
-		_in_offset(in_offset)
+		_processed_blocks(processed_blocks)
 	{
 	}
 
